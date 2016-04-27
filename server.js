c960c7787b59963d73fac7eb7da397ea8148033e
Mads Peter Jacobsen <madsissexy@gmail.com>
2016-04-27 20:29:31 +0200

    Add ?completed filter

--- server.js
+++ server.js
@@ -18,11 +18,22 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+// get todos?complated
 app.get('/todos', function (req, res) {
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
 	// convert to JSON and send back to who called it
-	res.json(todos);
+	res.json(filteredTodos);
 });
 
+// get todo
 app.get('/todos/:id', function (req, res) {
 	// params in urls are returned as "strings"
 	var todoId = parseInt(req.params.id, 10);
