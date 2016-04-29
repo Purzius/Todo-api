f6ce726e21e5c0877febce2cb08f7471b7e1f14e
Mads Peter Jacobsen <madsissexy@gmail.com>
2016-04-29 15:48:25 +0200

    Convert CRUD request to use current user todos only.

--- server.js
+++ server.js
@@ -24,7 +24,9 @@ app.get('/', function(req, res) {
 // get todos?complated=bool&q=description
 app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
-	var where = {};
+	var where = {
+		userId: req.user.get('id')
+	};
 
 	if (query.hasOwnProperty('completed') && query.completed === 'true') {
 		where.completed = true;
@@ -77,7 +79,12 @@ app.get('/todos', middleware.requireAuthentication, function(req, res) {
 app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	// params in urls are returned as "strings"
 	var todoId = parseInt(req.params.id, 10);
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (!!todo) {
 			res.json(todo.toJSON());
 		} else {
@@ -134,7 +141,8 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	db.todo.destroy({
 		where: {
-			id: todoId
+			id: todoId,
+			userId: req.user.get('id')
 		}
 	}).then(function(rowsDeleted) {
 		if (rowsDeleted === 0) {
@@ -162,7 +170,7 @@ app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	// }
 });
 
-// PUT
+// PUT /todos/:id
 app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var body = _.pick(req.body, 'description', 'completed')
@@ -177,7 +185,12 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	}
 
 	// Model method
-	db.todo.findById(todoId).then(function(todo) {
+	db.todo.findOne({
+		where: {
+			id: todoId,
+			userId: req.user.get('id')
+		}
+	}).then(function(todo) {
 		if (todo) {
 			todo.update(attributes).then(function(todo) {
 				res.json(todo.toJSON());
